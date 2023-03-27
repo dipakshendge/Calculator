6c65c73beae397e1193c543901323ec1f2fd2c9b
Dipak V Shendge <dipakshendge3360@gmail.com>
2023-03-28 00:03:15 +0530

    calculator

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-(function(){
+
+// console.log("hello")
+    let screen= document.querySelector('.screen')
+    let buttons=document.querySelectorAll(".btn")
+    let clear=  document.querySelector(".btn-clear")
+    let equal=  document.querySelector(".btn-equal")
+
+
+    buttons.forEach((button)=>{
+        button.addEventListener('click',function(e){
+            // console.log(e)
+            
+                if(e.target.dataset.num=="="){
+                    let ans =eval(screen.value);
+                    screen.value=ans;
+                }else{
+                    let result=e.target.dataset.num;
+                screen.value +=result
+                }
+            
+            // console.log(screen.value)
+        })
+        
+    });
     
-})
